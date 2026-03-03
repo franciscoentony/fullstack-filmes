fc6bffd037b8c671686a06580f5c89a8ac715f7a
Francisco Entony <franciscoentonydev@gmail.com>
2026-03-02 22:28:07 -0300

    atualizando projeto

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/fullstack-filmes/", // IMPORTANTE: Deve ser o nome do seu repositório no GitHub
 });
